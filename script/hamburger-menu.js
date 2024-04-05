e703a10112e8f33d08c1955905f6ec22310e2ebb
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2024-04-05 16:40:45 +0200

    Added overlay to mobile navigation

--- script/hamburger-menu.js
+++ script/hamburger-menu.js
@@ -1,12 +1,18 @@
 (() => {
   const hamburgerBtn = document.querySelector("#checkbox1");
   const mobileMenu = document.querySelector(".nav__mobileNav");
+  const overlay = document.querySelector(".overlay");
+  const body = document.body;
 
   hamburgerBtn.addEventListener("click", () => {
     if (hamburgerBtn.checked) {
       mobileMenu.style.display = "block";
+      overlay.classList.add("overlay--active");
+      body.style.overflow = "hidden";
     } else {
       mobileMenu.style.display = "none";
+      overlay.classList.remove("overlay--active");
+      body.style.overflow = "";
     }
   });
 })();
