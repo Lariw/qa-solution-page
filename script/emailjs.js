e096ac7613fc21169db4fd81bba1578c4aa639fd
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2024-04-15 19:18:39 +0200

    Added video off on mobile devices

--- script/emailjs.js
+++ script/emailjs.js
@@ -1,30 +1,40 @@
 const sendMail = (name, email, subject, message) => {
-  const serviceID = "service_294trqi";
-  const templateID = "template_u5zshdl";
-  let params = {
-    name,
-    email,
-    subject,
-    message,
-  };
-  emailjs.send(serviceID, templateID, params);
+  return new Promise((resolve, reject) => {
+    const serviceID = "service_294trqi";
+    const templateID = "template_u5zshdl";
+    let params = {
+      name,
+      email,
+      subject,
+      message,
+    };
+    emailjs
+      .send(serviceID, templateID, params)
+      .then(() => {
+        resolve();
+      })
+      .catch((error) => {
+        reject(error);
+      });
+  });
 };
 
 const form = document.querySelector(".js-form");
-form.addEventListener("submit", (e) => {
+form.addEventListener("submit", async (e) => {
   e.preventDefault();
   const name = document.querySelector(".js-name").value;
   const emailAddress = document.querySelector(".js-email").value;
   const subject = document.querySelector(".js-subject").value;
   const message = document.querySelector(".js-message").value;
-  sendMail(name, emailAddress, subject, message);
-  document.querySelector(".form__successMessage").style.display = "flex";
-  form.reset();
-  setTimeout(() => {
-    document.querySelector(".form__successMessage").style.display = "none";
-  }, 5000);
-});
 
-document.querySelector(".js-submit").addEventListener("click", function () {
-  document.querySelector(".form__successMessage").style.display = "flex";
+  try {
+    await sendMail(name, emailAddress, subject, message);
+    document.querySelector(".form__successMessage").style.display = "flex";
+    form.reset();
+    setTimeout(() => {
+      document.querySelector(".form__successMessage").style.display = "none";
+    }, 2000);
+  } catch (error) {
+    console.error(error);
+  }
 });
