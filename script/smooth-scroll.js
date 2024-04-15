1afd129288e70ed3ab19ff7ff977c9933b2feafe
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2024-04-15 13:51:32 +0200

    Changes in anchors

--- script/smooth-scroll.js
+++ script/smooth-scroll.js
@@ -1,1 +1,14 @@
-(()=>{let e=e=>{document.querySelector(e).scrollIntoView({behavior:"smooth"})};document.querySelectorAll(".nav__link, .header_btnLink").forEach(t=>{t.addEventListener("click",r=>{r.preventDefault(),e(t.getAttribute("href"))})})})();
+(() => {
+  let e = (e) => {
+    const element = document.querySelector(e);
+    const offset = element.getBoundingClientRect().top + window.scrollY;
+    window.scrollTo({ top: offset - 150, behavior: "smooth" });
+  };
+  document
+    .querySelectorAll(".nav__link, .header_btnLink, .smooth-scroll")
+    .forEach((t) => {
+      t.addEventListener("click", (r) => {
+        r.preventDefault(), e(t.getAttribute("href"));
+      });
+    });
+})();
