209b19d966234d55e9826b3810499de20c6ad4b9
Kamil Hajduła <98982966+Lariw@users.noreply.github.com>
2024-04-16 10:05:24 +0200

    Minify JS

--- script/smooth-scroll.js
+++ script/smooth-scroll.js
@@ -1,14 +1,1 @@
-(() => {
-  let e = (e) => {
-    const element = document.querySelector(e);
-    const offset = element.getBoundingClientRect().top + window.scrollY;
-    window.scrollTo({ top: offset - 150, behavior: "smooth" });
-  };
-  document
-    .querySelectorAll(".nav__link, .header_btnLink, .smooth-scroll")
-    .forEach((t) => {
-      t.addEventListener("click", (r) => {
-        r.preventDefault(), e(t.getAttribute("href"));
-      });
-    });
-})();
+(()=>{let e=e=>{let t=document.querySelector(e),l=t.getBoundingClientRect().top+window.scrollY;window.scrollTo({top:l-150,behavior:"smooth"})};document.querySelectorAll(".nav__link, .header_btnLink, .smooth-scroll").forEach(t=>{t.addEventListener("click",l=>{l.preventDefault(),e(t.getAttribute("href"))})})})();
